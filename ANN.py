684a2bc11e02eebec2e649cc0e2fbc902c5fbba6
King-Junco <brennankimbrell0601@gmail.com>
2025-03-31 15:43:23 -0500

    templet used
    inputed the templet used

---
+++ ANN.py
@@ -0,0 +1,83 @@
+# %% MODULE BEGINS
+module_name = 'ANN'
+
+'''
+Version: 1
+
+Description:
+    <***>
+
+Authors:
+    Brennan Kimbrell
+    Trent Law
+
+Date Created     :  3/31/2025
+Date Last Updated:  3/31/2025
+
+Doc:
+    <***>
+
+Notes:
+    <***>
+'''
+
+# %% IMPORTS                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == "__main__":
+    import os
+    # os.chdir("./../..")
+#
+
+# custom imports
+
+
+# other imports
+from copy import deepcopy as dpcpy
+
+'''
+from   matplotlib import pyplot as plt
+import mne
+import numpy  as np 
+import os
+import pandas as pd
+import seaborn as sns
+'''
+
+
+# %% USER INTERFACE              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# %% CONSTANTS                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# %% CONFIGURATION               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# %% INITIALIZATIONS             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# %% DECLARATIONS                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Global declarations Start Here
+
+
+# Class definitions Start Here
+
+
+# Function definitions Start Here
+def main():
+    pass
+
+
+#
+
+# %% MAIN CODE                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Main code start here
+
+
+# %% SELF-RUN                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Main Self-run block
+if __name__ == "__main__":
+    print(f"\"{module_name}\" module begins.")
+
+    # TEST Code
+    main()
